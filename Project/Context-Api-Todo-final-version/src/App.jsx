c4391d3435f6f0e6f59d257497c15e0967727b8f
Aniket Pathak <aniketpathak1370@gmail.com>
2024-01-20 16:51:20 +0530

    useReducer is applied

--- Project/Context-Api-Todo-final-version/src/App.jsx
+++ Project/Context-Api-Todo-final-version/src/App.jsx
@@ -3,45 +3,19 @@ import Appname from "./components'/AppName";
 import AddTodo from "./components'/AddTodo";
 import TodoItem from "./components'/TodoItem";
 import WelcomeMessage from "./components'/WelcomeMessege";
-import { TodoItemContext } from "./store/todo-item-store";
-
 import "./App.css";
-import { useState } from "react";
+import TodoItemContextProvider from "./store/todo-item-store";
 
 function App() {
-  const [todoItem, setTodoItem] = useState([]);
-
-  const addNewItem = (itemName, itemDueDate) => {
-    let newTodoItem = [
-      ...todoItem,
-      {
-        name: itemName,
-        dueDate: itemDueDate,
-      },
-    ];
-    setTodoItem(newTodoItem);
-  };
-
-  const deleteItem = (todoItemName) => {
-    const newItem = todoItem.filter((item) => item.name !== todoItemName);
-    setTodoItem(newItem);
-  };
-
   return (
-    <TodoItemContext.Provider
-      value={{
-        todoItem: todoItem,
-        addNewItem: addNewItem,
-        deleteItem: deleteItem,
-      }}
-    >
+    <TodoItemContextProvider>
       <div id="mainApp">
-        <Appname/>
-        <AddTodo/>
-        <WelcomeMessage/>
-        <TodoItem/>
+        <Appname />
+        <AddTodo />
+        <WelcomeMessage />
+        <TodoItem />
       </div>
-    </TodoItemContext.Provider>
+    </TodoItemContextProvider>
   );
 }
 
--- Project/Context-Api-Todo-final-version/src/store/todo-item-store.jsx
+++ Project/Context-Api-Todo-final-version/src/store/todo-item-store.jsx
@@ -1,4 +1,64 @@
-
 import { createContext } from "react";
+import { useReducer } from "react";
 
-export const TodoItemContext = createContext([]);
+export const TodoItemContext = createContext({
+  todoItem: [],
+  addNewItem: () => {},
+  deleteItem: () => {},
+});
+
+const todoItemReducer = (currTodoItemState, action) => {
+  let newTodoItem = currTodoItemState;
+  if (action.type === "NEW_ITEM") {
+    newTodoItem = [
+      ...currTodoItemState,
+      {
+        name: action.playload.itemName,
+        dueDate: action.playload.itemDueDate,
+      },
+    ];
+  } else if (action.type === "DELETE_ITEM") {
+    newTodoItem = currTodoItemState.filter(
+      (item) => item.name !== action.playload.itemName
+    );
+  }
+  return newTodoItem;
+};
+
+const TodoItemContextProvider = ({ children }) => {
+  const [todoItem, dispatchTodoItem] = useReducer(todoItemReducer, []);
+
+  const addNewItem = (itemName, itemDueDate) => {
+    const newItemAction = {
+      type: "NEW_ITEM",
+      playload: {
+        itemName,
+        itemDueDate,
+      },
+    };
+    dispatchTodoItem(newItemAction);
+  };
+
+  const deleteItem = (todoItemName) => {
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      playload: {
+        itemName: todoItemName,
+      },
+    };
+    dispatchTodoItem(deleteItemAction);
+  };
+  return (
+    <TodoItemContext.Provider
+      value={{
+        todoItem: todoItem,
+        addNewItem: addNewItem,
+        deleteItem: deleteItem,
+      }}
+    >
+      {children}
+    </TodoItemContext.Provider>
+  );
+};
+
+export default TodoItemContextProvider;
