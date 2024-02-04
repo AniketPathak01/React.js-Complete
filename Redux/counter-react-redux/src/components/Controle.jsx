52599d0100d8084d31600757ac8e681855128ab8
Aniket Pathak <aniketpathak1370@gmail.com>
2024-02-04 22:51:20 +0530

    Redux

---
+++ Redux/counter-react-redux/src/components/Controle.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Controle() {
+  return (
+    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+      <button type="button" class="btn btn-success">
+        +1
+      </button>
+      <button type="button" class="btn btn-danger">
+        -1
+      </button>
+    </div>
+  );
+}
+
+export default Controle;
---
+++ Redux/counter-react-redux/src/components/DisplayCounter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DisplayCounter() {
+  return <p class="lead mb-4">Counter current Value:</p>;
+}
+
+export default DisplayCounter;
